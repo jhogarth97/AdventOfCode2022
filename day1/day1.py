f3521cf0876ec847ff4c67a8336fcc248a324933
Joshua Hogarth <joshua.hogarth@metoffice.gov.uk>
2022-12-14 12:03:15 +0000

    Day 1 part 2 additions

--- day1/day1.py
+++ day1/day1.py
@@ -19,7 +19,7 @@ def read_list_and_list_of_elves_with_calories(calorie_intake_file: str) -> list:
         elves.append([int(x) for x in calories.split("\n")])
     return elves
     
-def find_the_most_calories_held_by_elf(elves_with_calories: list) -> Optional[int]:
+def find_the_most_calories_held_by_elf(elves_with_calories: list, number_of_backup_elves:int = 0) -> Optional[int]:
     """
     This method finds the highest calorie collection within all the elves.
     If there are no elves, the answer given is None.
@@ -27,12 +27,26 @@ def find_the_most_calories_held_by_elf(elves_with_calories: list) -> Optional[in
 
     if not elves_with_calories:
         return None
-    return max([sum(x) for x in elves_with_calories])
+
+    calories_summed_up_per_elf = sorted([sum(x) for x in elves_with_calories], reverse=True)[0:number_of_backup_elves+1]
+    
+    if number_of_backup_elves == 0:
+        return calories_summed_up_per_elf[0]
+    
+    return calories_summed_up_per_elf
+    
 
 
 if __name__ == "__main__":
 
     file = open("day1/elf_list.txt", "r", encoding="utf-8").read()
     elves = read_list_and_list_of_elves_with_calories(file)
+
+    # Day 1 part 1
     most_calories = find_the_most_calories_held_by_elf(elves)
     print(most_calories) # 69177
+
+    # Day 1 part 2
+    most_calories = find_the_most_calories_held_by_elf(elves, 2)
+    print(sum(most_calories)) # 207456
+
